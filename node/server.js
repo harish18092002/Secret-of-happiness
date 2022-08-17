672a6a48b223aca0c1de8ebdfcbcf5ec9b2a4a8c
Harish A <82990734+harish18092002@users.noreply.github.com>
2022-08-17 08:04:48 -0700

    Add files via upload

--- node/server.js
+++ node/server.js
@@ -48,11 +48,65 @@ res.end("Your data has been stored successfully")
   if(req.method==="PUT"){
      const path = req.url;
      const parts = path.split('/').slice(1);
-    if (usersDatabase)
-  console.log(parts)
+     console.log(parts)
+
+  let newDb = usersDatabase.forEach((element =>{
+    if(element.id===parts){
+      req.on("datas",(datas)=>{
+        element.id = datas;
+        element.name = datas;
+        element.year = datas;
+      });
+      req.on("end",()=>{
+        usersDatabase.push(newDb)
+        console.log(usersDatabase)
+      })
+      res.end("Your data is updated")
+    }
+  }))
+
+
+    
+  
   
   }
   
 });
 
 server.listen(1080);
+
+/* const arr = [
+    {
+        id: 1,
+        firstName: 'Abin',
+        lastName: 'Wc',
+        role: 'Software Engineer',
+    },
+    {
+        id: 2,
+        firstName: 'Siva',
+        lastName: 'Raman',
+        role: 'Software Engineer',
+    },
+    {
+        id: 3,
+        firstName: 'Harish',
+        lastName: 'A',
+        role: 'Software Engineer',
+    },
+    {
+        id: 4,
+        firstName: 'Harish',
+        lastName: 'Kumar',
+        role: 'Software Engineer',
+    },
+];
+const newarray= arr.forEach((element=>{
+
+if(element.id===3){
+   element.firstName= "Surya"
+    element.lastName= "prasad"
+    console.log(element)
+}
+}))
+ */

de7646025e3ab29eab018566911d6f53049adee3
Harish A <82990734+harish18092002@users.noreply.github.com>
2022-08-20 04:02:45 -0700

    Add files via upload

--- node/server.js
+++ node/server.js
@@ -1,27 +1,32 @@
 // Importing packages
 
 import http from "node:http";
-import editJsonFile from "edit-json-file";
-import asyn from "asyn";
-import stringToJson from "string-to-json";
-import { format } from "node:path";
-import axios from "axios";
-import { title } from "node:process";
+
 
 // POST method to post the users data
 
-let usersDatabase = []
+let usersDatabase =[];
 let userData;
 const server = http.createServer(function (req, res) {
   if(req.method === "POST" && req.url === "/create-user" ){
     req.setEncoding("utf8");
    req.on("data",(data)=>{
   userData = data;
+  // console.log(userData)
 });
 req.on("end",()=>{
-  usersDatabase.push(userData)
-  console.log(userData)
-  console.log(usersDatabase)
+
+(usersDatabase.push(JSON.parse(userData)))
+
+
+
+
+
+
+
+ console.log(usersDatabase, typeof usersDatabase)
+//  console.log(userData)
+  
   res.end("Your data has been stored successfully")
 })
   
@@ -35,7 +40,7 @@ req.on("end",()=>{
 // Get method to find and display users
 
   if(req.method==="GET" && req.url==="/find-user"){
-    usersDatabase.forEach((item,index)=>{
+ usersDatabase.forEach((item,index)=>{
       if(index===usersDatabase.length-1)
       res.write(item);
       else res.write(item + ",");
@@ -46,6 +51,7 @@ req.on("end",()=>{
 
 
 // PUT method to update the user data
+
 
   if(req.method==="PUT"){
      const path = req.url;
@@ -66,7 +72,7 @@ req.on("end",()=>{
           console.log(element.id,typeof element.id)
           
   
-          if (element.id === part){
+          if (element.id == part){
 console.log("hi")
             element.name = datas.name
 
@@ -84,38 +90,3 @@ console.log("hi")
 
 server.listen(1080);
 
-/* const arr = [
-    {
-        id: 1,
-        firstName: 'Abin',
-        lastName: 'Wc',
-        role: 'Software Engineer',
-    },
-    {
-        id: 2,
-        firstName: 'Siva',
-        lastName: 'Raman',
-        role: 'Software Engineer',
-    },
-    {
-        id: 3,
-        firstName: 'Harish',
-        lastName: 'A',
-        role: 'Software Engineer',
-    },
-    {
-        id: 4,
-        firstName: 'Harish',
-        lastName: 'Kumar',
-        role: 'Software Engineer',
-    },
-];
-const newarray= arr.forEach((element=>{
-
-if(element.id===3){
-   element.firstName= "Surya"
-    element.lastName= "prasad"
-    console.log(element)
-}
-}))
- */
